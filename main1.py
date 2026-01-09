76d4d47d7aaec46bd6b729dc97f183dc983e3aa9
1h3gamer <Idaune.nathan@gmail.com>
2026-01-09 17:47:32 +0000

    tkinter geometry

---
+++ main1.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+root=Tk()
+root.title("number Pad")
+root.geometry("250x300")
+
+nums=[
+    [9,8,7],
+    [6,5,4],
+    [3,2,1],
+    ["#",0,"*"]
+]
+
+for i in range(4):
+    root.rowconfigure(i,weight=1,minsize=50)
+for j in range(3):
+    root.columnconfigure(j,weight=1,minsize=75)
+
+for i in range(4):
+    for j in range(3):
+        frame=Frame(
+        master=root,
+        relief=SUNKEN,
+        borderwidth=1
+        )
+        frame.grid(row=i,column=j,padx=2,pady=2,sticky="nsew")
+
+        label=Label(
+            master=frame,
+            text=nums[i][j],
+            bg="white",
+            font=("Arial",14)
+        )
+        label.pack(expand=True,fill="both")
+
+root.mainloop()
+
